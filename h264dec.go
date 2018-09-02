58d3ce65bdeecabca162db27ed1534dba584eb62
Kai H <kai.hagelstein@gmail.com>
2018-09-02 17:45:01 +0200

    Test new code

--- h264dec.go
+++ h264dec.go
@@ -2,9 +2,11 @@ package h264
 
 import (
 	/*
-		#include <libavcodec/avcodec.h>
-		#include <libavformat/avformat.h>
-		#include <libavutil/avutil.h>
+		#cgo linux,amd64 pkg-config: libav_linux_amd64.pc
+
+		#include "libavcodec/avcodec.h"
+		#include "libavutil/avutil.h"
+		#include "libavformat/avformat.h"
 
 		typedef struct {
 			AVCodec *c;
@@ -15,20 +17,92 @@ import (
 
 		static int h264dec_new(h264dec_t *h, uint8_t *data, int len) {
 			h->c = avcodec_find_decoder(AV_CODEC_ID_H264);
+
 			h->ctx = avcodec_alloc_context3(h->c);
-			h->f = av_frame_alloc();
 			h->ctx->extradata = data;
 			h->ctx->extradata_size = len;
-			h->ctx->debug = 0x3;
+			h->f = av_frame_alloc();
+
 			return avcodec_open2(h->ctx, h->c, 0);
+		}
+
+		static int vp8_new(h264dec_t *h) {
+			h->c = avcodec_find_decoder(AV_CODEC_ID_VP8);
+
+			h->ctx = avcodec_alloc_context3(h->c);
+			h->ctx->refcounted_frames = 1;
+			h->f = av_frame_alloc();
+
+			return avcodec_open2(h->ctx, h->c, 0);
+		}
+
+		static int h264dec_release(h264dec_t *m) {
+			// release context
+			avcodec_close(m->ctx);
+			av_free(m->ctx);
+
+			// release frame
+			av_frame_free(&m->f);
 		}
 
 		static int h264dec_decode(h264dec_t *h, uint8_t *data, int len) {
 			AVPacket pkt;
 			av_init_packet(&pkt);
-			pkt.data = data;
+
+			if (data != NULL) {
+				pkt.data = data;
+				pkt.size = len;
+			}
+
+			int r = avcodec_decode_video2(h->ctx, h->f, &h->got, &pkt);
+
+			static char error_buffer[255];
+			if (r < 0) {
+				av_strerror(r, error_buffer, sizeof(error_buffer));
+				av_log(h->ctx, AV_LOG_DEBUG, "Video decode error: %s\n", error_buffer);
+			}
+
+			return r;
+		}
+
+		static int h264dec_decode2(h264dec_t *h, uint8_t *data, int len, int64_t pts, int64_t dts, AVFrame *frame) {
+			int r;
+			static char error_buffer[255];
+			AVPacket pkt;
+
+			av_init_packet(&pkt);
+
+			//pkt.data = data;
 			pkt.size = len;
-			return avcodec_decode_video2(h->ctx, h->f, &h->got, &pkt);
+			pkt.pts  = pts;
+			pkt.dts  = dts;
+
+			// copy go memory first
+			// allocate memory by av_malloc
+			// use size with FF_INPUT_BUFFER_PADDING_SIZE
+			// create reference counted buffer
+			void *new_packet_data;
+			new_packet_data = av_malloc(len+ FF_INPUT_BUFFER_PADDING_SIZE);
+			memset(new_packet_data + len, 0, FF_INPUT_BUFFER_PADDING_SIZE);
+			memcpy(new_packet_data, data, len);
+
+			r = av_packet_from_data(&pkt, (uint8_t *)new_packet_data, len);
+			if (r < 0) {
+				av_strerror(r, error_buffer, sizeof(error_buffer));
+				av_log(h->ctx, AV_LOG_DEBUG, "Video decode error: %s\n", error_buffer);
+
+				return r;
+			}
+
+
+			r = avcodec_decode_video2(h->ctx, frame, &h->got, &pkt);
+
+			if (r < 0) {
+				av_strerror(r, error_buffer, sizeof(error_buffer));
+				av_log(h->ctx, AV_LOG_DEBUG, "Video decode error: %s\n", error_buffer);
+			}
+
+			return r;
 		}
 	*/
 	"C"
@@ -36,25 +110,52 @@ import (
 	"image"
 	"unsafe"
 )
+import "log"
 
-type Decoder struct {
+type H264Decoder struct {
 	m C.h264dec_t
 }
 
-func NewDecoder(header []byte) (m *Decoder, err error) {
-	m = &Decoder{}
+func NewH264Decoder(header []byte) (m *H264Decoder, err error) {
+	m = &H264Decoder{}
+
+	avLock.Lock()
+	defer avLock.Unlock()
+
 	r := C.h264dec_new(
 		&m.m,
 		(*C.uint8_t)(unsafe.Pointer(&header[0])),
 		(C.int)(len(header)),
 	)
+
 	if int(r) < 0 {
 		err = errors.New("open codec failed")
 	}
+
 	return
 }
 
-func (m *Decoder) Decode(nal []byte) (f *image.YCbCr, err error) {
+func NewVP8Decoder() (m *H264Decoder, err error) {
+	m = &H264Decoder{}
+
+	avLock.Lock()
+	defer avLock.Unlock()
+
+	r := C.vp8_new(&m.m)
+	if int(r) < 0 {
+		err = errors.New("open codec failed")
+	}
+
+	return
+}
+
+func (m *H264Decoder) Release() {
+	log.Printf("Decoder released")
+
+	C.h264dec_release(&m.m)
+}
+
+func (m *H264Decoder) Decode(nal []byte) (f *image.YCbCr, err error) {
 	r := C.h264dec_decode(
 		&m.m,
 		(*C.uint8_t)(unsafe.Pointer(&nal[0])),
@@ -83,6 +184,34 @@ func (m *Decoder) Decode(nal []byte) (f *image.YCbCr, err error) {
 		SubsampleRatio: image.YCbCrSubsampleRatio420,
 		Rect:           image.Rect(0, 0, w, h),
 	}
+
+	return
+}
+
+func (m *H264Decoder) Decode2(avPacket *AVPacket, avFrame *AVFrame) (got bool, err error) {
+	p := (*C.uint8_t)(unsafe.Pointer(nil))
+	l := (C.int)(0)
+	if avPacket.Data != nil {
+		p = (*C.uint8_t)(unsafe.Pointer(&avPacket.Data[0]))
+		l = (C.int)(len(avPacket.Data))
+	}
+
+	r := C.h264dec_decode2(
+		&m.m,
+		p,
+		l,
+		(C.int64_t)(avPacket.Pts),
+		(C.int64_t)(avPacket.Dts),
+		avFrame.f,
+	)
+
+	if int(r) < 0 {
+		err = errors.New("decode failed")
+
+		return
+	}
+
+	got = m.m.got > 0
 
 	return
 }
